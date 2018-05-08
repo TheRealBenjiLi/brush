7a4234b7b691448c7e03e0c4b74c25147c295428
Benjamin Li <benlibenli@gmail.com>
2018-05-08 16:28:30 -0700

    TestBoss now flies and shoots bullets downard at random. TestBoss also moves left and right.

---
+++ Assets/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	public int health;
+	public int damage;
+	public Rigidbody2D rb;
+
+	private void isKilled() {
+		if (health <= 0) {
+			// Remove object
+		}
+	}
+}
---
+++ Assets/Test Chamber/TestBoss.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBoss : Entity {
+
+	public int t;
+	public Vector2 direction;
+	public Vector3 offset;
+	private int prevBulletTime;
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		health = 10;
+		damage = 1;
+		rb = GetComponent<Rigidbody2D>();
+		t = 0;
+		prevBulletTime = t;
+		direction = Vector2.left * 2f;
+		offset = Vector3.down * 0.5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Move(t);
+		Attack(t, rb);
+		t++;
+	}
+
+	void Move (int t) {
+		if (t % 300 == 0) {
+			rb.velocity = direction;
+			direction *= -1;
+		}
+	}
+
+	void Attack (int t, Rigidbody2D rb) {
+		if (t - prevBulletTime > 50 && Random.value > 0.95) {
+			var bullet = (GameObject) Instantiate(bulletPrefab, rb.transform.position + offset, rb.transform.rotation);
+			bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+			Destroy(bullet, 5.0f);
+			prevBulletTime = t;
+		}
+	}
+}
