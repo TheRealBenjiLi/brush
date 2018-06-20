f9eeb13adcc9323d658db8be193d2a2d1bdd38a3
Benjamin Li <benlibenli@gmail.com>
2018-06-19 22:01:58 -0700

    Bug fix: jumps now reset even when jump button is held down

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,9 +9,9 @@ public class Player : Entity {
 	protected float speed;
 	protected float jumpMultiplier;
 	protected int t;
-	protected int prevJumpStart;
-	protected int maxJumpsRemaining;
-	protected int jumpsRemaining;
+	protected int prevJumpStart;               // Used to calculate jump arcs
+	protected int maxJumpsRemaining;           // Maximum jumps for the player
+	protected int jumpsRemaining;              // Counter to track jumps remaining
 	protected bool faceDirection;              // Left is false, right is true
 	protected Vector3 attackOffsetHorizontal;  // Offset is for facing right
 	protected Vector3 attackOffsetVertical;    // Offset is for facing up
@@ -40,6 +40,7 @@ public class Player : Entity {
 		float y = rb.velocity.y;
 		if (Input.GetButtonDown("Jump") && jumpsRemaining > 0) {
 			prevJumpStart = t;
+			jumpsRemaining--;
 		}
 		if (Input.GetButton("Jump")) {
 			float jumpNormal = 1.0f - 0.1f * (t - prevJumpStart);
@@ -49,21 +50,20 @@ public class Player : Entity {
 		}
 		if (Input.GetButtonUp("Jump") && jumpsRemaining > 0) {
 			y = 0;
-			jumpsRemaining--;
 		}
 		return y;
 	}
 
 	// Current purpose is to reset jumps when contacting the top of a
 	// "Ground" object.
-	// TODO: is there a better way to construct colScale?
+	// TODO: is there a better way to construct colToPlayer and colScale?
 	public void OnCollisionEnter2D (Collision2D col) {
-		Vector2 dir = gameObject.GetComponent<Renderer>().bounds.center -
+		Vector2 colToPlayer = gameObject.GetComponent<Renderer>().bounds.center -
 			col.gameObject.GetComponent<Renderer>().bounds.center;
 		Vector2 colScale = new Vector2(col.gameObject.transform.lossyScale.x,
 			col.gameObject.transform.lossyScale.y);
-		dir = new Vector2(dir.x / colScale.x, dir.y / colScale.y);
-		if (col.gameObject.tag == "Ground" && dir.y > Mathf.Abs(dir.x)) {
+		colToPlayer = new Vector2(colToPlayer.x / colScale.x, colToPlayer.y / colScale.y);
+		if (col.gameObject.tag == "Ground" && colToPlayer.y > Mathf.Abs(colToPlayer.x)) {
 			jumpsRemaining = maxJumpsRemaining;
 		}
 	}
