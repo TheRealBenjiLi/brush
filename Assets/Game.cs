dac395e6d2d1d7af224f5ada5e4b1fc930312724
Benjamin Li <benlibenli@gmail.com>
2018-07-03 21:05:38 -0700

    Initialize save state properties. Todo: how to save player upgrades

---
+++ Assets/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game {
+
+	public static Game currentGame;
+	public int externalCurrency;
+
+	public Game () {
+		externalCurrency = 0;
+	}
+}
