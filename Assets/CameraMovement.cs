5753ed714fd70340c0ce3ac6d40c0847514ca7ed
Benjamin Li <benlibenli@gmail.com>
2018-07-08 13:37:07 -0700

    Hub initialized. Camera script created to handle room-to-room camera movement. New LevelBlocks folder with a few worldbuilding prefabs

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+
+	// Update is called once per frame
+	void Update () {
+		float playerX = player.transform.position.x;
+		float playerY = player.transform.position.y;
+		transform.position = new Vector3(
+			Mathf.Floor((playerX + 7.0f) / 14.0f) * 14.0f,
+			Mathf.Floor((playerY + 4.0f) / 8.0f) * 8.0f, -10.0f);
+	}
+}
