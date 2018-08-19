8d39832daa1036b3eb8f42720628a9f38b74d726
TheRealBenjiLi <benlibenli@gmail.com>
2018-08-19 13:32:44 -0700

    Fix Styling on QuitOnClick.cs

--- Assets/MainMenu/QuitOnClick.cs
+++ Assets/MainMenu/QuitOnClick.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuitOnClick : MonoBehaviour {
-	public void Quit() {
+	public void Quit () {
 		#if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
 		#else
