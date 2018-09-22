da9b8b3548b23c3ab114ca088f56e7397784f662
Benjamin Li <benlibenli@gmail.com>
2018-09-22 16:10:36 -0700

    Bugfix: splitBoss now attacks properly

--- Assets/Bosses/SplitBoss/SplitBoss.cs
+++ Assets/Bosses/SplitBoss/SplitBoss.cs
@@ -82,9 +82,11 @@ public class SplitBoss : Entity {
 			return;
 		}
 		if (col.gameObject.tag == "Ground" &&
-			Physics2D.Raycast(transform.position, Vector2.down).collider.gameObject == col.gameObject) {
+			Physics2D.Raycast(transform.position, Vector2.down,
+			Mathf.Infinity, 9).distance <= 1.0f) {
 			lastAction = t;
 			grounded = true;
+			print(grounded);
 		} else {
 			rb.velocity = new Vector2(0, 0);
 		}
